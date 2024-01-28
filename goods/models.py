6488c511ea9eb3a70afd6f3869c40c8b12929b82
arty <av.yaz@yandex.ru>
2024-01-28 13:58:53 +0300

    Added fixtures and formed the database with data from fixtures

--- goods/models.py
+++ goods/models.py
@@ -1,3 +1,54 @@
+from email.policy import default
+from enum import unique
+from re import T
+from unicodedata import category
 from django.db import models
 
+
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(
+        max_length=100, unique=True, verbose_name="Название категории"
+    )
+    slug = models.SlugField(
+        max_length=100, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+
+    class Meta:
+        db_table = "category"
+        verbose_name = "категорию"
+        verbose_name_plural = "категории"
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Products(models.Model):
+    name = models.CharField(
+        max_length=100, unique=True, verbose_name="Название категории"
+    )
+    slug = models.SlugField(
+        max_length=100, unique=True, blank=True, null=True, verbose_name="URL"
+    )
+    description = models.TextField(blank=True, null=True, verbose_name="Описание")
+    image = models.ImageField(
+        upload_to="goods_images", blank=True, null=True, verbose_name="Изображение"
+    )
+    price = models.DecimalField(
+        default=0.00, max_digits=7, decimal_places=2, verbose_name="Цена"
+    )
+    discount = models.DecimalField(
+        default=0.00, max_digits=4, decimal_places=2, verbose_name="Скидка в процентах"
+    )
+    quantity = models.PositiveIntegerField(default=0, verbose_name="Количество")
+    category = models.ForeignKey(
+        to=Categories, on_delete=models.CASCADE, verbose_name="Категория"
+    )
+
+    class Meta:
+        db_table = "product"
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+
+    def __str__(self):
+        return f'(self.name) Количесвто - {self.quantity}'
