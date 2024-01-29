4a50dc76f91492118c8b88ac29684bf492830653
arty <av.yaz@yandex.ru>
2024-01-29 20:48:25 +0300

    Added product filter

--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,5 +1,6 @@
 from distutils.command import register
 from django import template
+from django.utils.http import urlencode
 from goods.models import Categories
 
 register = template.Library()
@@ -7,3 +8,9 @@ register = template.Library()
 @register.simple_tag(name="tag_categories")
 def tag_categories():
     return Categories.objects.all()
+
+@register.simple_tag(name="change_params", takes_context=True)
+def change_params(context, **kwargs):
+    query = context['request'].GET.dict()
+    query.update(kwargs)
+    return urlencode(query)
