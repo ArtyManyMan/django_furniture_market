ef0350b49f25b4cf629cbf841c1ac503d2874769
arty <av.yaz@yandex.ru>
2024-01-30 00:10:21 +0300

    Added searching by name and description

--- goods/urls.py
+++ goods/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 from goods import views as views_goods
 
+from django.db.models import Q
+
 app_name = "goods"
 
 urlpatterns = [
+    path('search/', views_goods.catalog, name="search"),
     path("<slug:category_slug>/", views_goods.catalog, name="index"),
     path("product/<slug:product_slug>/", views_goods.product, name="product"),
 ]
