e5de1324239fe4f6e97136dab1732448abf8a6f8
arty <av.yaz@yandex.ru>
2024-02-03 19:50:32 +0300

    Added cart remove controller

--- carts/urls.py
+++ carts/urls.py
@@ -7,5 +7,5 @@ app_name = "carts"
 urlpatterns = [
     path('cart_add/<slug:product_slug>', views_carts.cart_add, name="cart_add"),
     path('cart_change/<slug:produc_tslug>', views_carts.cart_change, name="cart_change"),
-    path('cart_remove/<slug:product_slug>', views_carts.cart_remove, name="cart_remove"),
+    path('cart_remove/<int:cart_id>', views_carts.cart_remove, name="cart_remove"),
 ]
