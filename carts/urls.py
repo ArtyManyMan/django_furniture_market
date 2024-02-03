105b4d08b422b86aee5190516d876c065096534a
arty <av.yaz@yandex.ru>
2024-02-03 18:08:51 +0300

    Added cart models(api), views and refactored templates

--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,7 @@ from carts import views as views_carts
 app_name = "carts"
 
 urlpatterns = [
-    path('cart_add/<int:product_id>', views_carts.cart_add, name="cart_add"),
-    path('cart_change/<int:product_id>', views_carts.cart_change, name="cart_change"),
-    path('cart_remove/<int:product_id>', views_carts.cart_remove, name="cart_remove"),
+    path('cart_add/<slug:product_slug>', views_carts.cart_add, name="cart_add"),
+    path('cart_change/<slug:produc_tslug>', views_carts.cart_change, name="cart_change"),
+    path('cart_remove/<slug:product_slug>', views_carts.cart_remove, name="cart_remove"),
 ]
