618dcbc14374a4403758d5b12bba920999dae9b5
arty <av.yaz@yandex.ru>
2024-02-04 21:36:10 +0300

    Added ajax for carts controllers

--- carts/urls.py
+++ carts/urls.py
@@ -5,7 +5,7 @@ from carts import views as views_carts
 app_name = "carts"
 
 urlpatterns = [
-    path('cart_add/<slug:product_slug>', views_carts.cart_add, name="cart_add"),
-    path('cart_change/<slug:produc_tslug>', views_carts.cart_change, name="cart_change"),
-    path('cart_remove/<int:cart_id>', views_carts.cart_remove, name="cart_remove"),
+    path('cart_add/', views_carts.cart_add, name="cart_add"),
+    path('cart_change/', views_carts.cart_change, name="cart_change"),
+    path('cart_remove/', views_carts.cart_remove, name="cart_remove"),
 ]
